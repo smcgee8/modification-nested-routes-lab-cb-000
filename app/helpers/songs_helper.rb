ad66d5d7c4b0898aa5f661074be21ef595682a1f
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 17:34:15 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,6 @@
 module SongsHelper
   def artist_select(song, form_type)
-    if song.artist.nil?
+    if song.artist.nil? || form_type == :direct_edit
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist_id
