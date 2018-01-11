c739fb9ae6e18cd923164e8e4f1272b9e3440354
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 17:35:48 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+  def artist_select(song, form_type)
+    if song.artist.nil? || form_type == :direct_edit
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+      display_artist(song)
+    end
+  end
 end
