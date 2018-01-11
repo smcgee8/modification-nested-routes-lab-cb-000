cc7e8ce6a9aed41bfb130ecd534c02ac4e91a97c
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-11 17:35:15 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -4,6 +4,7 @@ module SongsHelper
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist_id
+      display_artist(song)
     end
   end
 end
